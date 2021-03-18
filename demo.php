1f2046f75af7f261e1e76d8c2a09222fe98db15a
sampurnapuchaa <80377757+sampurnapuchaa@users.noreply.github.com>
2021-03-17 23:03:32 -0700

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+$firstname=$_POST["firstname"];
+$surtname=$_POST["surname"];
+$email=$_POST["email"];
+$mobilenumber=$_POST["mobilenumber"];
+$newpassword=$_POST["newpassword"];
+echo "welcome" .$firstname; 
+?>
